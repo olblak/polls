92f237707fc0a41b426c392472a8157aa07191ad
Olblak <me@olblak.com>
2019-10-25 10:52:57 +0200

    Only run db migration if env variable provided

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/olblak/polls/pkg/db"
 	"github.com/olblak/polls/pkg/http"
+	"os"
+	"string"
 )
 
 func main() {
-	db.RunDatabaseMigration()
+	if migration_enabled := os.Getenv("DB_MIGRATION_ENABLED"); strings.ToLower(migration_enabled) == "true" {
+		db.RunDatabaseMigration()
+	}
 	http.StartHttp()
 }
